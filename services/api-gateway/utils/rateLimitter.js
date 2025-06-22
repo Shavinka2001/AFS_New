2a9bd74da2fc127731b063506a5dd140ab8d42e7
Pawarasasmina <pawarasasmina2002@icloud.com>
2025-06-22 22:40:22 +0530

    rate

--- services/api-gateway/utils/rateLimitter.js
+++ services/api-gateway/utils/rateLimitter.js
@@ -2,6 +2,6 @@ const {rateLimit} = require('express-rate-limit')
 
 //Rate limiting
 exports.applyRateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    limit:100,
+    windowMs: 60 * 60 * 1000,
+    limit:1000,
   })
