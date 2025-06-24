ff276fafae13b7ba5e9de5b20e07dde53c387600
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-06-24 23:11:12 +0530

    Refactor auth service initialization and improve CORS configuration

--- services/Auth/server.js
+++ services/Auth/server.js
@@ -1,18 +1,20 @@
 // This file serves as an entry point to start the auth service
-require('./src/server.js');
+const express = require('express');
 const cors = require('cors');
+const app = express();
 
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'] // <-- Add PATCH here
+}));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS'); // <-- Add PATCH
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  next();
+});
 
-
-
-
-
-
-
-
-}); next();  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS'); // <-- Add PATCH  res.header('Access-Control-Allow-Credentials', 'true');  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');app.use((req, res, next) => {}));}));
+require('./src/server.js');
